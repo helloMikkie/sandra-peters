f660410443b308495832051441ab04fbd34be770
helloMikkie <mh@groenlandberlin.de>
2019-12-19 14:22:21 +0100

    Major Update Prototype

---
+++ js/script.js
@@ -0,0 +1,12 @@
+  
+      var iso = new Isotope( '.masonry-grid', {
+        itemSelector: '.item_work',
+        percentPosition: true,
+        layoutMode: 'packery',
+        transitionDuration: '0.2s',
+
+        packery: {
+          columnWidth: '.grid-sizer',
+          gutter: '.gutter-sizer',
+        }
+      })
